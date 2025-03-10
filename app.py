080d7ae2790f82ff4b40391adb297878cd57a50c
RusevCreateX <radoslav.rusev.createx@gmail.com>
2025-03-10 13:37:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import openai
 import os
 
 # -- Заменете това с Вашия OpenAI API ключ! --
-openai.api_key = "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"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # -- Може да промените този System Prompt според Вашите нужди --
 SYSTEM_PROMPT = """
